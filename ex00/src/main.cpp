94e4ac2115b3545732b07612e531838ca1c2c96a
Serhii Veretennikov <gridis72@gmail.com>
2026-06-08 16:25:25 +0200

    feat: added flag to main bin compilation

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -4,12 +4,16 @@
 
 int main() {
 	
-	std::vector<int> vec = {1, 2, 3, 4};
+	std::vector<int> vec;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
 
 	std::vector<int>::const_iterator vecRes = vec.end();
 	
 	try {
-		vecRes = easyfind(vec, 0);
+		vecRes = easyfind(vec, -1);
 	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
