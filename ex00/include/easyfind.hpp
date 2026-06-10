62efcc3462b6deb43ab0eb8c7dc513a6ed14ea9c
Serhii Veretennikov <gridis72@gmail.com>
2026-06-10 15:16:18 +0200

    refactor: changed exception

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -1,15 +1,8 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-# include <exception>
+# include <stdexcept>
 # include <algorithm>
-
-class NotFoundExeption: public std::exception {
-	public:
-		virtual const char *what() const throw() {
-			return ("Value is not in the container");
-	}
-};
 
 template <typename T>
 typename T::iterator easyfind(T &container, int val) {
@@ -17,7 +10,7 @@ typename T::iterator easyfind(T &container, int val) {
 
 	res = find(container.begin(), container.end(), val);
 	if (res == container.end())
-		throw NotFoundExeption();
+		throw std::runtime_error("Value is not in the container");
 
 	return (res);
 }
